4a49ce499997eb89f38b8b99ecc8c6a99c818165
Phuc Tran <thienphucst92@gmail.com>
2017-05-30 19:39:23 +0700

    Modify search form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,8 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
-    if params[:search]
-      @posts = Post.search(params[:search]).order("created_at DESC")
+    if params[:search_post]
+      @posts = Post.search(params[:search_post]).order("created_at DESC")
     else
       @posts = Post.all.order("created_at DESC")
     end
